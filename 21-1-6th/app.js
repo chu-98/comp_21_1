ee029892c2d628be1c5225a2425cbefc9a7d3136
chu-98 <81007078+chu-98@users.noreply.github.com>
2021-05-19 16:52:59 +0900

    21.05.19(수) 6주차 (3)
    역시 처음은 낯설고 어색하다.
    계속해서 반복하고 따라하자.
    그러면서 실력이 는다.

--- 21-1-6th/app.js
+++ 21-1-6th/app.js
@@ -1,16 +1,39 @@
 const express = require("express");
+const { get } = require("http");
+const path = require("path")
 
 const app = express();
 
+app.set('port', process.env.PORT || 8080);
+
+app.use((req, res, next) => {
+    console.log("모든 요청")
+    next()
+})
+
+
+
 app.get('/', (req, res) => {
-    res.send("익스프레스 배워보기")
+    res.sendFile(path.join(__dirname, "./client/list.html"))
 })
 
 app.get('/about', (req, res) => {
     res.send("어바웃")
 })
 
-app.listen(8080, () => {
-    console.log("서버 실행 중...");
+app.get('/:id', (req, res) => {
+    res.send(`${req.params.id}`)
 })
 
+app.use((req, res) => {
+    res.send("404 NOT FOUND")
+})
+
+app.use((err, req, res, next) => {
+    console.log(err);
+    res.send("서버 터짐")
+})
+
+app.listen(app.get('port'), () => {
+    console.log("서버 실행 중...");
+})
